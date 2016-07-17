3129cba3a7cec0de26b5878b0a28a126a9243ae7
lcamilo <lcamilo@esec.com.br>
2016-07-17 17:23:10 -0300

    master: Implementado page static no projeto
    - O arquivo express.js foi organizado
    - Comecando a configuracao da sessao

--- config/express.js
+++ config/express.js
@@ -2,29 +2,45 @@ var express = require('express'),
     morgan = require('morgan').
     compress = require('compression'),
     bodyParser = require('body-parser'),
-    methodOverride = require('method-override');
+    methodOverride = require('method-override'),
+    session = require('express-session');
 
 
 module.exports = function(){
   var app = express();
-  if(process.env.NODE_ENV === 'deploymonent'){
-    app.use(morgan('dev'));
-  }else if(process.env.NODE_ENV === 'production'){
-    app.use(compress());
-  }
 
-  app.use(bodyParser.urlencoded({
-    extended : true
-  }));
+  configureEnvironment()
 
-  app.use(bodyParser.json());
-  app.use(methodOverride());
+  configureBodyParser(app);
 
-  app.set('views','./app/views');
-  app.set('view engine','ejs');
+  configureViewPages(app);
 
   require('../app/routes/index.server.routes.js')(app);
 
   app.use(express.static('./public'));
   return app;
+};
+
+var configureBodyParser = function(app){
+  app.use(bodyParser.urlencoded({
+    extended : true
+  }));
+  
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+
+};
+
+var configureViewPages = function(app){
+  app.set('views','./app/views');
+  app.set('view engine','ejs');
+};
+
+
+var configureEnvironment = function(app){
+  if(process.env.NODE_ENV === 'deploymonent'){
+    app.use(morgan('dev'));
+  }else if(process.env.NODE_ENV === 'production'){
+    app.use(compress());
+  }
 };
